07b4375a96626889144bb6e412c82c164d2e8399
AntonioCLRJ <juniiorsouza2004@gmail.com>
2026-07-29 17:24:00 -0300

    fix: Adiciona a coluna email como Unique

--- src/main/java/dev/java/futFlow/Jogadores/JogadorModel.java
+++ src/main/java/dev/java/futFlow/Jogadores/JogadorModel.java
@@ -16,11 +16,16 @@ public class JogadorModel {
 
     private Long id;
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private Posicao posicao;
     private String nacionalidade;
+
     private int numeroDaCamisa;
     private int idade;
+
     @ManyToOne //O jogador tem apenas um clube.
     @JoinColumn(name = "clube_id") //Chave estrangeira
     private ClubeModel clube;

77e2dab09ae3c6540bece1dff7d29fd52f7fe159
AntonioCLRJ <juniiorsouza2004@gmail.com>
2026-07-29 17:06:09 -0300

    feat: Adiciona o lombok na classe para abstração

--- src/main/java/dev/java/futFlow/Clubes/ClubeModel.java
+++ src/main/java/dev/java/futFlow/Clubes/ClubeModel.java
@@ -2,11 +2,18 @@ package dev.java.futFlow.Clubes;
 
 import dev.java.futFlow.Jogadores.JogadorModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_clubes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class ClubeModel {
 
     @Id
@@ -19,45 +26,5 @@ public class ClubeModel {
     @OneToMany(mappedBy = "clube") //Um clube pode ter vários jogadores.
     private List<JogadorModel> jogador;
 
-    public ClubeModel() {
-    }
-
-    public ClubeModel(String nomeClube, String federacao, int anoDeCriacao, String pais) {
-        this.nomeClube = nomeClube;
-        this.federacao = federacao;
-        this.anoDeCriacao = anoDeCriacao;
-        this.pais = pais;
-    }
-
-    public String getNomeClube() {
-        return nomeClube;
-    }
-
-    public String getFederacao() { return federacao; }
-
-    public int getAnoDeCriacao() {
-        return anoDeCriacao;
-    }
-
-    public String getPais() { return pais; }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public void setPais(String pais) { this.pais = pais; }
-
-    public void setNomeClube(String nomeClube) {
-        this.nomeClube = nomeClube;
-    }
-
-    public void setFederacao(String federacao) {
-        this.federacao = federacao;
-    }
-
-    public void setAnoDeCriacao(int anoDeCriacao) {
-        this.anoDeCriacao = anoDeCriacao;
-    }
-
 }
+
--- src/main/java/dev/java/futFlow/Jogadores/JogadorModel.java
+++ src/main/java/dev/java/futFlow/Jogadores/JogadorModel.java
@@ -1,13 +1,16 @@
 package dev.java.futFlow.Jogadores;
 import dev.java.futFlow.Clubes.ClubeModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor //constructor no args
+@AllArgsConstructor //constructor all args
+@Data //getters - setters
 public class JogadorModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
@@ -21,71 +24,5 @@ public class JogadorModel {
     @ManyToOne //O jogador tem apenas um clube.
     @JoinColumn(name = "clube_id") //Chave estrangeira
     private ClubeModel clube;
-
-
-    public JogadorModel(String nome, String email, Posicao posicao, String nacionalidade, int numeroDaCamisa, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.posicao = posicao;
-        this.nacionalidade = nacionalidade;
-        this.numeroDaCamisa = numeroDaCamisa;
-        this.idade = idade;
-    }
-
-    public JogadorModel() {
-        //no args constructor
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Posicao getPosicao() {
-        return posicao;
-    }
-
-    public void setPosicao(Posicao posicao) {
-        this.posicao = posicao;
-    }
-
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
-
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
-    }
-
-    public int getNumeroDaCamisa() {
-        return numeroDaCamisa;
-    }
-
-    public void setNumeroDaCamisa(int numeroDaCamisa) {
-        this.numeroDaCamisa = numeroDaCamisa;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Long getId(){
-        return id;
-    }
 
 }

4974d139fd19228f9c38dc34a04270b48d9ea66c
AntonioCLRJ <juniiorsouza2004@gmail.com>
2026-08-03 16:50:47 -0300

    chore: inclui a versão do DTO pela entidade e mapeia manualmente no JogadorMapper os caminhos Entidade X DTO

--- src/main/java/dev/java/futFlow/Jogadores/JogadorDTO.java
+++ src/main/java/dev/java/futFlow/Jogadores/JogadorDTO.java
@@ -1,4 +1,23 @@
 package dev.java.futFlow.Jogadores;
+import dev.java.futFlow.Clubes.ClubeModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 
 public class JogadorDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private Posicao posicao;
+    private String nacionalidade;
+    private int numeroDaCamisa;
+    private int idade;
+    private ClubeModel clube;
+
 }
--- src/main/java/dev/java/futFlow/Jogadores/JogadorMapper.java
+++ src/main/java/dev/java/futFlow/Jogadores/JogadorMapper.java
@@ -1,4 +1,33 @@
 package dev.java.futFlow.Jogadores;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JogadorMapper {
+
+    public JogadorModel map(JogadorDTO jogadorDTO) {
+        JogadorModel jogadorModel = new JogadorModel();
+        jogadorModel.setId(jogadorDTO.getId());
+        jogadorModel.setNome(jogadorDTO.getNome());
+        jogadorModel.setEmail(jogadorDTO.getEmail());
+        jogadorModel.setIdade(jogadorDTO.getIdade());
+        jogadorModel.setNacionalidade(jogadorDTO.getNacionalidade());
+        jogadorModel.setNumeroDaCamisa(jogadorDTO.getNumeroDaCamisa());
+        jogadorModel.setPosicao(jogadorDTO.getPosicao());
+        jogadorModel.setClube(jogadorDTO.getClube());
+        return  jogadorModel;
+    }
+
+    public JogadorDTO map(JogadorModel jogadorModel) {
+        JogadorDTO jogadorDTO = new JogadorDTO();
+        jogadorDTO.setId(jogadorModel.getId());
+        jogadorDTO.setNome(jogadorModel.getNome());
+        jogadorDTO.setEmail(jogadorModel.getEmail());
+        jogadorDTO.setIdade(jogadorModel.getIdade());
+        jogadorDTO.setNacionalidade(jogadorModel.getNacionalidade());
+        jogadorDTO.setNumeroDaCamisa(jogadorModel.getNumeroDaCamisa());
+        jogadorDTO.setPosicao(jogadorModel.getPosicao());
+        jogadorDTO.setClube(jogadorModel.getClube());
+        return jogadorDTO;
+    }
 }
